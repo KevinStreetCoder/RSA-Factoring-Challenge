b4535bedde1e3d7c1e55bf2d84bf31c6a6872a96
KevinStreetCoder <kevinisaackareithi@gmail.com>
2023-06-23 14:54:37 +0300

    RSA commits

---
+++ factors.py
@@ -0,0 +1,38 @@
+import sys
+import math
+
+def factorize(number):
+    factors = []
+    # Check divisibility by 2
+    while number % 2 == 0:
+        factors.append(2)
+        number //= 2
+
+    # Check divisibility by odd numbers starting from 3
+    divisor = 3
+    while divisor <= math.isqrt(number):
+        if number % divisor == 0:
+            factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 2
+
+    if number > 2:
+        factors.append(number)
+
+    return factors
+
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            number = int(line.strip())
+            factors = factorize(number)
+            print(f'{number}={"*".join(map(str, factors))}')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: factors <file>')
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    process_file(file_path)
---
+++ rsa.py
@@ -0,0 +1,45 @@
+import sys
+import math
+
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, math.isqrt(number) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+def factorize(number):
+    factors = []
+    # Check divisibility by 2
+    while number % 2 == 0:
+        factors.append(2)
+        number //= 2
+
+    # Check divisibility by odd primes starting from 3
+    divisor = 3
+    while divisor <= math.isqrt(number):
+        if number % divisor == 0 and is_prime(divisor):
+            factors.append(divisor)
+            number //= divisor
+        else:
+            divisor += 2
+
+    if number > 2:
+        factors.append(number)
+
+    return factors
+
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        number = int(file.readline().strip())
+        factors = factorize(number)
+        print(f'{number}={"*".join(map(str, factors))}')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: rsa <file>')
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    process_file(file_path)
